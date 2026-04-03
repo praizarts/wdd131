6254ecfa6197b6494abc8a72ee1c7c8dd90a009c
John Mark <mbachuignatia@gmail.com>
2026-04-03 23:44:55 +0100

    update

--- project/scripts/main.js
+++ project/scripts/main.js
@@ -86,3 +86,16 @@ document.getElementById("currentyear").textContent = new Date().getFullYear();
   document.getElementById("lastModified").textContent =
     "Last Modified: " + document.lastModified;
 
+
+    // Hamburger Menu Toggle
+document.addEventListener("DOMContentLoaded", () => {
+
+  const hamburger = document.getElementById("hamburger");
+  const navMenu = document.getElementById("navMenu");
+
+  hamburger.addEventListener("click", () => {
+    navMenu.classList.toggle("open");
+    hamburger.classList.toggle("active");
+  });
+
+});
